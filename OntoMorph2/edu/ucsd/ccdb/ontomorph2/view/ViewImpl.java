99252c7ce4b4dcbca45fc8bb55ef1da7cb329a2c
caprea <caprea@f77bb479-6037-0410-a603-adf5e3efd7e0>
2008-04-22 01:53:36 +0000

    rotation about the z-axis

--- OntoMorph2/edu/ucsd/ccdb/ontomorph2/view/ViewImpl.java
+++ OntoMorph2/edu/ucsd/ccdb/ontomorph2/view/ViewImpl.java
@@ -48,6 +48,7 @@ import com.jme.renderer.Renderer;
 import com.jme.scene.Node;
 import com.jme.scene.Spatial;
 import com.jme.scene.TriMesh;
+import com.jme.scene.CameraNode;
 import com.jme.scene.batch.GeomBatch;
 import com.jme.scene.shape.Box;
 import com.jme.scene.shape.Cylinder;
@@ -62,6 +63,7 @@ import com.jme.util.TextureManager;
 import com.jme.util.export.binary.BinaryImporter;
 import com.jme.util.geom.BufferUtils;
 import com.jme.util.geom.Debugger;
+import com.jmex.model.collada.schema.angularType;
 import com.jmex.model.converters.FormatConverter;
 import com.jmex.model.converters.MaxToJme;
 
@@ -95,12 +97,19 @@ public class ViewImpl extends BaseSimpleGame implements IView{
 	float coordDelta;
 	private SceneImpl _scene = null;
 
+	CameraNode camNode;			//thisobject needed for manipulating the camera in a simple way
 	AbsoluteMouse amouse; 	//the mouse object ref to entire screen
 	PickData prevPick;		//made global because it's a conveiniant way to deselect the previous selection since it's stored
 	
+	
+	//TODO: these variables track the direction of the camera - why can't the cameranode track this info itself?
+	float view_angleX=0;
+	float view_angleY=0;
+	float view_angleZ=0;
+	
 	org.fenggui.Display disp; // FengGUI's display
 
-//	there are two kinds of input, the FPS input and also FENG
+	//there are two kinds of input, the FPS input and also FENG
 	FengJMEInputHandler menuinput;	
 	
 	View3DImpl view3D = null;
@@ -127,6 +136,24 @@ public class ViewImpl extends BaseSimpleGame implements IView{
 		_scene = scene;
 	}
 	
+
+	//This function provides a conveiniance to update view_angleX/Y/Z 
+	//it also keeps it within bounds of 360
+	private float changeAngle(float angle, float angleFactor)
+	{
+	   /* recalculate rotation for the cylinder */
+        if (tpf < 1) //time_per_frame is provided
+        {
+            angle += (1 * (tpf * angleFactor));
+            if (angle > 360)
+            {
+            	angle = angle - 360;
+            }
+            if (angle < 0 ) angle = 0;	//error checking, just in case
+        }
+
+		return angle;
+	}
 	
 	protected void simpleInitGame() {
 		//as a hack, calling the main application class to do initialization
@@ -135,15 +162,14 @@ public class ViewImpl extends BaseSimpleGame implements IView{
 		
 		rootNode.attachChild(view3D);
 		
-		//This sphere is for debugging purposes, need to see something to indicate
+		
+		//This sphere is for debugging purposes, need to see something to indicate cam space/rotation
 		Sphere s=new Sphere("My sphere",10,10,20f);
-		// Do bounds for the sphere, but we'll use a BoundingBox this time
+		// Do bounds for the sphere, use a BoundingBox
 		s.setModelBound(new BoundingBox());
 		s.updateModelBound();
-		// Give the sphere random colors
 		s.setRandomColors();
 		s.setLocalTranslation(80,0,0);
-		//s.setSolidColor(ColorRGBA.blue);
 		
 		rootNode.attachChild(s);
 
@@ -194,31 +220,47 @@ public class ViewImpl extends BaseSimpleGame implements IView{
 		display.getRenderer().setBackgroundColor(ColorRGBA.black);
 		
 		
+		
+		//====================================
+		// CAMERA SETUP
+		//====================================
+		
 		///** Set up how our camera sees. */
 		cam.setFrustumPerspective(45.0f, (float) display.getWidth() / (float) display.getHeight(), 1, 1000);
 		
-		
 		//a locaiton on the Z axis a ways away
-		Vector3f loc = new Vector3f(0.0f, 0.0f, 200.0f);
-		Vector3f left = new Vector3f(-1.0f, 0.0f, 0.0f);
-		Vector3f up = new Vector3f(0.0f, 1.0f, 0.0f);
-		Vector3f dir = new Vector3f(0.0f, 0f, -1.0f);
+		Vector3f loc = new Vector3f(0.0f, 0.0f, -200.0f);
+		//Vector3f left = new Vector3f(-1.0f, 0.0f, 0.0f);
+		//Vector3f up = new Vector3f(0.0f, 1.0f, 0.0f);
+		//Vector3f dir = new Vector3f(0.0f, 0f, -1.0f);
 
 		///** Move our camera to a correct place and orientation. */
 		//pX, pY, pZ, orientation
-		cam.setFrame(loc, left, up, dir);
+		//cam.setFrame(loc, left, up, dir); //commented out because now using a camNode
 		
 		///** Signal that we've changed our camera's location/frustum. */
 		cam.update();
-		
+
 		///** Assign the camera to this renderer.*/
 		display.getRenderer().setCamera(cam);
+		
+		//camnode is for easy manipulation of the camera
+		camNode = new CameraNode("camera node", cam);
+		camNode.setLocalTranslation(loc);
+		
+		
+		rootNode.attachChild(camNode);
+		
+		//camNode.setLocalTranslation(loc);
+		System.out.println("Rotation: " + camNode.getLocalRotation() + "\nTranslation: " + camNode.getLocalTranslation());
+	
+		
+		//===================================================
 		
 		//the code for keybindings was previously here, it's been seperated for code readability
 		//Section for setting up the mouse and other input controls	
 		configureControls();
 		
-
 		//Remove lighting for rootNode so that it will use our basic colors
 		rootNode.setLightCombineMode(LightState.OFF);
 		
@@ -283,8 +325,8 @@ public class ViewImpl extends BaseSimpleGame implements IView{
 		//Bind the Escape key to kill our test app
 		KeyBindingManager.getKeyBindingManager().set("quit", KeyInput.KEY_ESCAPE);
 		
-		//assign 'R' to reload the view to inital state
-		KeyBindingManager.getKeyBindingManager().set("reinit", KeyInput.KEY_R);
+		//assign 'R' to reload the view to inital state //reinit
+		KeyBindingManager.getKeyBindingManager().set("reset", KeyInput.KEY_R);
 		
 		//assignt he camera to up, down, left, right
 		KeyBindingManager.getKeyBindingManager().set("cam_forward", KeyInput.KEY_UP);
@@ -310,119 +352,102 @@ public class ViewImpl extends BaseSimpleGame implements IView{
 
 	private void handleInput() 
 	{
-		//handle mouse input
-		{
-			if (MouseInput.get().isButtonDown(0)) 
-			{
-				
-				PickResults pr = new TrianglePickResults();
-				
-				//Get the position that the mouse is pointing to
-	            Vector2f mPos = new Vector2f();
-	            mPos.set(MouseInput.get().getXAbsolute() ,MouseInput.get().getYAbsolute() );
-	     
-	            // Get the world location of that X,Y value
-	            Vector3f farPoint = display.getWorldCoordinates(mPos, 1.0f);
-	            Vector3f closePoint = display.getWorldCoordinates(mPos, 0.0f);
-
-	            
-	            // Create a ray starting from the camera, and going in the direction
-	            // of the mouse's location
-	            Ray mouseRay = new Ray(cam.getLocation(), farPoint.subtractLocal(closePoint).normalizeLocal());
-	            //Ray mouseRay = new Ray(cam.getLocation(), closePoint);
-	            
-	            // Does the mouse's ray intersect the box's world bounds?
-	            pr.clear();
-	            pr.setCheckDistance(true); 			//this function is undocumented
-	            rootNode.findPick(mouseRay, pr);
-			
-	            //set up for deselection
-				if ( pr.getNumber() > 0)
-				{
-					//deselect the previous 
-					if ( prevPick != null) prevPick.getTargetMesh().setRandomColors();
-		           
-					for (int i = 0; i < pr.getNumber(); i++)
-					{
-						PickData item= pr.getPickData(i);
-						GeomBatch thing = item.getTargetMesh();
-						//thing.setRandomColors();
-						//thing.setSolidColor(ColorRGBA.cyan);
-					}
-					
-					
-					//find the one that is closest
-					//the 0th element is closest to the origin of the ray with checkdistance
-					//This is the distance from the origin of the Ray to the nearest point on the BoundingVolume of the Geometry.
-					prevPick = pr.getPickData(0);	//take the closest pick and set
-					prevPick.getTargetMesh().setSolidColor(ColorRGBA.yellow);
-				}
-				
-				//createLine(closePoint, farPoint); //debugging
-				//createSphere(closePoint); //for debugging
-			}
-		}
-		
-		
 		//key input handle
 		{
 			//exit the program cleanly on ESC
 			if (isAction("quit")) finish();
 			
-			if ( isAction("cam_forward")) 	cam.setLocation( cam.getLocation().add(0,0,1.1f) );			
-			
-			if ( isAction("cam_back"))		cam.setLocation( cam.getLocation().add(0,0,-1.1f) );
-			
-			if ( isAction("cam_turn_ccw"))	
-			{ //left key
-				{
-					/* This quaternion stores a 180 degree rolling rotation */ 
-					 Quaternion roll = new Quaternion(); 
-					 int a = 5;
-					 roll.fromAngleAxis( FastMath.PI * a /180 , new Vector3f(0,0,1) ); //rotates a degrees 
-					 
-					 
-					 
-					 rootNode.setLocalRotation(roll);;
-				}
+			if ( isAction("cam_forward")) 
+			{
+				camNode.setLocalTranslation( camNode.getLocalTranslation().add(0,0,1.1f));
 			}
+			
+			if ( isAction("cam_back"))
+			{
+				camNode.setLocalTranslation( camNode.getLocalTranslation().add(0,0,-1.1f));
+			}
+
 			
 			if ( isAction("cam_turn_cw"))	
 			{
 				//key right
-	       		Quaternion q = new Quaternion();
-	       		Vector3f unit = new Vector3f();
-	       		Vector3f dir = new Vector3f();
-	       		
-	       		dir = cam.getDirection();
-	       		//unit = new Vector3f(1f,0f,0f);
-	       		
-            	
-	       		unit = unit.normalize();
-            	
-        		q.lookAt(unit, cam.getUp());
-        		
-        		cam.setAxes(q);
-				
-				
-
+				 Quaternion roll = new Quaternion(); 
+				 view_angleZ = changeAngle(view_angleZ, 500);					 
+				 roll.fromAngleAxis( FastMath.PI * view_angleZ / 180 , Vector3f.UNIT_Z ); //rotates a degrees 
+				 camNode.setLocalRotation(roll);
+			}
+			
+			
+			if ( isAction("cam_turn_ccw"))	
+			{ //left key
+					 Quaternion roll = new Quaternion(); 
+					 view_angleZ = changeAngle(view_angleZ, -500);					 
+					 roll.fromAngleAxis( FastMath.PI * view_angleZ / 180 , Vector3f.UNIT_Z ); //rotates a degrees 
+					 camNode.setLocalRotation(roll);
 			}
 			
 			if ( isAction("info"))
 			{
-				logger.log(Level.INFO, "\nLocation: " + cam.getLocation().toString() + 
-						"\nDirection: " + cam.getDirection().toString());						
+				logger.log(Level.INFO, 
+						"\nAxes: " + "" +
+						"\nLoc Rotation: " + camNode.getLocalRotation() +
+						"\nWorld Rot: "+ camNode.getWorldRotation() + 
+						"\nDirection: " + camNode.getCamera().getDirection() +
+						"\nLoc Trans: "+ camNode.getLocalTranslation() +
+						"\nWorld Trans: "+ camNode.getWorldTranslation());
 			}
 			
 			if ( isAction("reset"))
 			{
 				logger.log(Level.INFO, "\nResetting");
+				Quaternion q = new Quaternion();
+				q.fromAxes(Vector3f.UNIT_X, Vector3f.UNIT_Y,Vector3f.UNIT_Z);
+				camNode.setLocalRotation(q);
 			}
 			
+		}//end key input
+		
+		
+		//handle mouse input
+		if (MouseInput.get().isButtonDown(0)) 
+		{
 			
-			logger.log(Level.FINEST, cam.getDirection().toString() );
+			PickResults pr = new TrianglePickResults();
 			
-		}	
+			//Get the position that the mouse is pointing to
+            Vector2f mPos = new Vector2f();
+            mPos.set(MouseInput.get().getXAbsolute() ,MouseInput.get().getYAbsolute() );
+     
+            // Get the world location of that X,Y value
+            Vector3f farPoint = display.getWorldCoordinates(mPos, 1.0f);
+            Vector3f closePoint = display.getWorldCoordinates(mPos, 0.0f);
+            
+            // Create a ray starting from the camera, and going in the direction
+            // of the mouse's location
+            Ray mouseRay = new Ray(closePoint, farPoint.subtractLocal(closePoint).normalizeLocal());
+            
+            // Does the mouse's ray intersect the box's world bounds?
+            pr.clear();
+            pr.setCheckDistance(true); 			//this function is undocumented
+            rootNode.findPick(mouseRay, pr);
+		
+			//createLine(closePoint, farPoint); //debugging
+			//createSphere(closePoint); //for debugging
+            
+            //set up for deselection
+			if ( pr.getNumber() > 0)
+			{
+				//deselect the previous 
+				if ( prevPick != null) prevPick.getTargetMesh().setRandomColors();
+				
+				//find the one that is closest
+				//the 0th element is closest to the origin of the ray with checkdistance
+				//This is the distance from the origin of the Ray to the nearest point on the BoundingVolume of the Geometry.
+				prevPick = pr.getPickData(0);	//take the closest pick and set
+				prevPick.getTargetMesh().setSolidColor(ColorRGBA.yellow);
+				System.out.println("Picked: " + prevPick.getTargetMesh().getName());
+			} //end if of pr > 0
+		} //end if mouse button down
 	}
 	
 	
