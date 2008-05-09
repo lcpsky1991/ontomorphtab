27b7b7189a7da73c86dda4ca8a902e7c5b9c69d7
stephen.larson <stephen.larson@f77bb479-6037-0410-a603-adf5e3efd7e0>
2008-05-09 02:45:21 +0000

    removing the view/misc directory because the classes are unused

--- OntoMorph2/src/edu/ucsd/ccdb/ontomorph2/view/misc/EntityViewer.java
+++
@@ -1,6 +0,0 @@
-package edu.ucsd.ccdb.ontomorph2.view.misc;
-public class EntityViewer {
-
-			private edu.ucsd.ccdb.ontomorph2.view.misc.ImageViewer lnkImageViewer;
-		private edu.ucsd.ccdb.ontomorph2.view.misc.MorphologyViewer lnkMorphologyViewer;
-}
