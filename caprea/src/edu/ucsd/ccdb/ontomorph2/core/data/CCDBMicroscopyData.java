3984c5129c569b774f9c765e20a890786b150520
caprea <caprea@f77bb479-6037-0410-a603-adf5e3efd7e0>
2008-06-17 00:44:44 +0000

    deleting caprea branch

--- caprea/src/edu/ucsd/ccdb/ontomorph2/core/data/CCDBMicroscopyData.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsd.ccdb.ontomorph2.core.data;
-
-public abstract class CCDBMicroscopyData implements java.io.Serializable {
-
-	private String projectLeader;
-	/** add other microscopy product data here */
-}
