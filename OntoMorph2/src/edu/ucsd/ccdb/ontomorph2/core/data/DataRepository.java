1867f4d2e9392f60b2053a03e7dbdda67aff5d66
caprea <caprea@f77bb479-6037-0410-a603-adf5e3efd7e0>
2008-10-06 08:22:50 +0000

    1. save session XML and persistent slides

--- OntoMorph2/src/edu/ucsd/ccdb/ontomorph2/core/data/DataRepository.java
+++ OntoMorph2/src/edu/ucsd/ccdb/ontomorph2/core/data/DataRepository.java
@@ -15,7 +15,7 @@ import org.morphml.neuroml.schema.XWBCTangible;
 
 
 
-/**
+/** .
  * Wraps a database that stores all the information about current position/rotation/scale of objects.
  * Also keeps track of users, changes, layers, and user preferences.
  * @author Stephen D. Larson (slarson@ncmir.ucsd.edu)
